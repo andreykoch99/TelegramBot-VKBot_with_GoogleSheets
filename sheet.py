a8afe72026bd0fe2c0f7fd8f0a37fec0a64d238f
Den <maldenser@yandex.ru>
2020-10-02 17:11:43 +0300

    добавление создание локального списка с именами

--- sheet.py
+++ sheet.py
@@ -2,34 +2,24 @@ import gspread
 from pprint import pprint
 
 gc = gspread.service_account(filename='credentials.json')
-sh = gc.open_by_key('1UNZorZ_QGAVFU3rXtjcHL9i3LOV9Y4dEWBrxk0r0CHA')
+sh = gc.open_by_key('TOKEN')
 worksheet = sh.sheet1
 
 res = worksheet.get_all_records()
-print(type(res))
-# pprint(res)
-pprint(type(res[0]['vk']))
-# pprint(res[0].items())
-# pprint(dict(res))
-# pprint(res[0])
-find_me = 'maldenser@yandex.ru'
-# if find_me in res:
+# print(type(res))
+pprint(type(res[0]['vk'])) # <class 'str'>
+pprint(res[0].items())
+n = len(res)
+i = 0
+our_list  = []
+while n != i:
+    our_list.append(res[i]['имя'])
+    i += 1
+pprint(our_list)
+
+
+
+# if find_me in res[0]:
 #     pprint('Нашёл!')
 # else:
 #     pprint('Не нашёл!')
-
-# pprint(res)
-# pprint(type(res))
-# pprint(worksheet.findall('maldenser@yandex'))
-#
-# def find_element(tree, element_name):
-#     if element_name in tree:
-#         return tree[element_name]
-#     for key, sub_tree in tree.items():
-#         if isinstance(sub_tree, dict):
-#             result = find_element(tree=sub_tree, element_name=element_name)
-#             if result:
-#                 break
-#     else:
-#         result = None
-#     return result
