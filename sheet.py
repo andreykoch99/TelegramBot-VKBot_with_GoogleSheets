873026227ddd0532e82fc319c2f70cac24823ba0
Den <maldenser@yandex.ru>
2020-10-03 15:16:12 +0300

    Стёрты лишние перемены, рефактор переменных

--- sheet.py
+++ sheet.py
@@ -2,30 +2,28 @@ import gspread
 
 
 def connect_and_get_data():
-    gc = gspread.service_account(filename='credentials.json')
-    sh = gc.open_by_key('TOKEN')
-    worksheet = sh.sheet1
-    res = worksheet.get_all_records()
+    res = gspread.service_account(filename='credentials.json').open_by_key(
+        'TOKEN').sheet1.get_all_records()
     return res
 
 
 def find_number(data):
-    n = len(data)
-    ans = []
+    number_of_rows = len(data)
+    answer = []
     print('Привет! По какому ключу (буквы фамилиии, имени) мы ищем?')
     find_me = input('!!! Будь внимателен, я чувствителен к регистру!!! (заглавные буквы, с которых начинаются имя и'
                     ' фамилия)\n')
     print('Начинаю поиск по ключу "{}"!'.format(find_me))
-    for i in range(0, n):
+    for i in range(0, number_of_rows):
         if data[i]['имя'].find(find_me) != -1:
-            ans.append(i)
+            answer.append(i)
             # print('Нашёл, это {}, её номер телефона: {}'.format(data[i]['имя'], data[i]['телефон']))
-    if len(ans) == 1:
-        print('Нашёл, это {}, её номер телефона: {}'.format(data[ans[0]]['имя'], data[ans[0]]['телефон']))
-    elif len(ans) != 1 and len(ans) > 1:
-        print('У меня есть {} вариантов, надеюсь, здесь есть то, что тебе нужно!'.format(len(ans)))
-        for j in range(0, len(ans)):
-            print('Вариант №{}, это {}, номер телефона: {}'.format(j + 1, data[ans[j]]['имя'], data[ans[j]]['телефон']))
+    if len(answer) == 1:
+        print('Нашёл, это {}, её номер телефона: {}'.format(data[answer[0]]['имя'], data[answer[0]]['телефон']))
+    elif len(answer) != 1 and len(answer) > 1:
+        print('У меня есть {} вариантов, надеюсь, здесь есть то, что тебе нужно!'.format(len(answer)))
+        for j in range(0, len(answer)):
+            print('Вариант №{}, это {}, номер телефона: {}'.format(j + 1, data[answer[j]]['имя'], data[answer[j]]['телефон']))
 
 
 find_number(connect_and_get_data())
