35ee183e2dd4684a3bf58f712201b00212c00ac7
Den <maldenser@yandex.ru>
2020-10-06 13:19:45 +0300

    connect_get_data_find.py: Добавлен ответ, если по базе ничего не будет найдено bot.py: 1) переработали логику, добавлена команда find, следующим шагом для которой будет ввод ключа 2) /help и /start дают одинаковый ответ

--- bot.py
+++ bot.py
@@ -1,23 +1,32 @@
 import telebot
 from connect_get_data_find import connect_and_get_data, find_number
 
-bot = telebot.TeleBot('TOKEN');
+bot = telebot.TeleBot('TOKEN')
 
-# Обработчик команд '/start' и '/help'.
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(func=lambda message: True, commands=['start', 'help'])
 def handle_start_help(message):
-    bot.send_message(message.chat.id, 'Привет! Я ищу номер телефона в базе конактов по ключу. '
-                                      'По какому ключу начинаем поиск?')
+    if message.text == '/help' or '/start':
+        bot.send_message(message.chat.id, 'Привет! Я ищу номер телефона в базе конактов по ключу. '
+                                          'Для старта напишите, например "/find"')
 
-@bot.message_handler(content_types=['text'])
-def get_key(message):
-    bot.send_message(message.chat.id, 'Начинаю поиск по ключу "{}"!'.format(message.text))
+
+@bot.message_handler(func=lambda message: True, commands=['find'])
+def handle_find(message):
+        bot.send_message(message.chat.id, 'Окей, теперь напиши имя или фамилию, по которой будем искать.'
+                                          'Будь внимателен, учитывай, что фамилия и имя должны начинаться с большой'
+                                          'буквы, чтобы я корректно нашёл. Также ты можешь ввести часть '
+                                          'имени или фамилии')
+        bot.register_next_step_handler(message, found_result)
+
+
+def found_result(message):
     key = message.text
+    bot.send_message(message.chat.id, 'Начинаю поиск по ключу "{}"!'.format(message.text))
     answer = find_number(connect_and_get_data(), key)
     for i in range(0, len(answer)):
         bot.send_message(message.chat.id, answer[i])
-
+    bot.send_message(message.chat.id, 'Если хочешь, мы можем попробовать снова. Просто напиши /find')
 
 
 bot.polling(none_stop=True, interval=0)
-
--- probe.py
+++ probe.py
@@ -1,29 +1,31 @@
-html_dom = {
-    'html': {
-        'head': {
-            'title': 'Колобок',
-        },
-        'body': {
-            'h2': 'Привет!',
-            'div': 'Хочешь, я расскажу тебе сказку?',
-            'p': 'Жили-были старик со старухой...',
-        }
-    }
-}
-
-
-def find_element(tree, element_name):
-    if element_name in tree:
-        return tree[element_name]
-    for key, sub_tree in tree.items():
-        if isinstance(sub_tree, dict):
-            result = find_element(tree=sub_tree, element_name=element_name)
-            if result:
-                break
+name = '';
+surname = '';
+age = 0;
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, "Как тебя зовут?");
+        bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
     else:
-        result = None
-    return result
+        bot.send_message(message.from_user.id, 'Напиши /reg');
 
+def get_name(message): #получаем фамилию
+    global name;
+    name = message.text;
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?');
+    bot.register_next_step_handler(message, get_surnme);
 
-res = find_element(tree=html_dom, element_name='tit')
-print(res)
+def get_surname(message):
+    global surname;
+    surname = message.text;
+    bot.send_message('Сколько тебе лет?');
+    bot.register_next_step_handler(message, get_age);
+
+def get_age(message):
+    global age;
+    while age == 0: #проверяем что возраст изменился
+        try:
+             age = int(message.text) #проверяем, что возраст введен корректно
+        except Exception:
+             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+      bot.send_message(message.from_user.id, 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?')
